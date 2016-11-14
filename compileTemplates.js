0983b695ab57fb32fe65ae80d9251c84658c39e5
Anthony Garvan <anthony.garvan@gsa.gov>
2016-11-14 13:43:32 -0600

    adding transaction workflow

--- compileTemplates.js
+++ compileTemplates.js
@@ -5,27 +5,38 @@ function read(path, tmpl) {
 }
 
 function compile() {
-  var breadcrumbs = read('templates/breadcrumbs.tmpl.html');
+  var breadcrumbsHome = read('templates/breadcrumbs-home.tmpl.html');
   var committeeForm = read('templates/committee-form.tmpl.html');
   var rightPane = 'test content';
   var header = read('templates/splash-header.tmpl.html');
   var landing = read('templates/landing.tmpl.html');
   var existingReport = read('templates/existing-report.tmpl.html');
+  var reportHeader = read('templates/report-header.tmpl.html')
+  var chooseTransactionType = read('templates/transaction-type.tmpl.html')
+  var breadCrumbsExistingReport = read('templates/breadcrumbs-existing-report.tmpl.html')
 
-  var index = read('templates/master.tmpl.html', {breadcrumbs: "",
+  var index = read('templates/master.tmpl.html', {breadcrumbs: breadcrumbsHome,
                                                     leftPane: landing,
                                                     rightPane: "",
                                                     header: header});
 
-  var existingReport = read('templates/master.tmpl.html', {breadcrumbs: "",
+  var selectExistingReport = read('templates/master.tmpl.html', {breadcrumbs: breadcrumbsHome,
                                                             leftPane: landing,
                                                             rightPane: existingReport,
                                                             header: header});
 
+  var selectTransactionType = read('templates/master.tmpl.html', {breadcrumbs: breadCrumbsExistingReport,
+                                                            leftPane: chooseTransactionType,
+                                                            rightPane: "",
+                                                            header: reportHeader});
+
   fs.writeFileSync('index.html', index);
   console.log('written: index.html');
-  fs.writeFileSync('html/existing-report.html', existingReport);
-  console.log('written: html/existing-report.html')
+  fs.writeFileSync('html/select-existing-report.html', selectExistingReport);
+  console.log('written: html/select-existing-report.html')
+  fs.writeFileSync('html/select-transaction-type.html', selectTransactionType);
+  console.log('written: html/select-transaction-type.html')
+
 }
 
 fs.watch('templates', {recursive: true}, compile)
