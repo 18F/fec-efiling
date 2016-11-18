a023764ddd9840016a40a349b1ba319ee8aa4db9
Anthony Garvan <anthony.garvan@gsa.gov>
2016-11-18 14:11:09 -0600

    fixing links to be able to run locally

--- compileTemplates.js
+++ compileTemplates.js
@@ -5,17 +5,15 @@ function read(path, tmpl) {
 }
 
 function compile() {
-  var breadcrumbsHome = read('templates/breadcrumbs-home.tmpl.html');
   var committeeForm = read('templates/committee-form.tmpl.html');
   var rightPane = 'test content';
   var header = read('templates/splash-header.tmpl.html');
-  var landing = read('templates/landing.tmpl.html');
   var existingReport = read('templates/existing-report.tmpl.html');
   var reportHeader = read('templates/report-header.tmpl.html')
   var chooseTransactionType = read('templates/transaction-type.tmpl.html')
   var breadCrumbsExistingReport = read('templates/breadcrumbs-existing-report.tmpl.html')
-  var chooseReceiptEntryType = read('templates/entry-type.tmpl.html', {manualEntryLink: '/html/enter-receipt.html'})
-  var chooseDisbursementEntryType = read('templates/entry-type.tmpl.html', {manualEntryLink: '/html/enter-disbursement.html'})
+  var chooseReceiptEntryType = read('templates/entry-type.tmpl.html', {manualEntryLink: 'enter-receipt.html'})
+  var chooseDisbursementEntryType = read('templates/entry-type.tmpl.html', {manualEntryLink: 'enter-disbursement.html'})
   var receiptContacts = read('templates/receipt-contact-types.tmpl.html')
   var disbursementContacts = read('templates/disbursement-contact-types.tmpl.html')
   var enterReceipt = read('templates/enter-transaction.tmpl.html', {contactTypes: receiptContacts,
@@ -25,45 +23,53 @@ function compile() {
   var readyToSubmitPane = read('templates/ready-to-submit.tmpl.html')
   var table = read('templates/table.tmpl.html')
 
-  var index = read('templates/master.tmpl.html', {breadcrumbs: breadcrumbsHome,
-                                                    leftPane: landing,
+  var index = read('templates/master.tmpl.html', {breadcrumbs: read('templates/breadcrumbs-home.tmpl.html', {rootDir: ''}),
+                                                    leftPane: read('templates/landing.tmpl.html', {rootDir: ''}),
                                                     rightPane: "",
-                                                    header: header});
+                                                    header: header,
+                                                    rootDir: ''});
 
-  var selectExistingReport = read('templates/master.tmpl.html', {breadcrumbs: breadcrumbsHome,
-                                                            leftPane: landing,
+  var selectExistingReport = read('templates/master.tmpl.html', {breadcrumbs: read('templates/breadcrumbs-home.tmpl.html', {rootDir: '../'}),
+                                                            leftPane: read('templates/landing.tmpl.html', {rootDir: '../'}),
                                                             rightPane: existingReport,
-                                                            header: header});
+                                                            header: header,
+                                                            rootDir: '../'});
 
   var selectTransactionType = read('templates/master.tmpl.html', {breadcrumbs: breadCrumbsExistingReport,
                                                             leftPane: chooseTransactionType,
                                                             rightPane: "",
-                                                            header: reportHeader});
+                                                            header: reportHeader,
+                                                            rootDir: '../'});
 
   var selectReceiptEntryType = read('templates/master.tmpl.html', {breadcrumbs: breadCrumbsExistingReport,
                                                             leftPane: chooseReceiptEntryType,
                                                             rightPane: "",
-                                                            header: reportHeader});
+                                                            header: reportHeader,
+                                                            rootDir: '../'});
 
   var selectDisbursementEntryType = read('templates/master.tmpl.html', {breadcrumbs: breadCrumbsExistingReport,
                                                             leftPane: chooseDisbursementEntryType,
                                                             rightPane: "",
-                                                            header: reportHeader});
+                                                            header: reportHeader,
+                                                            rootDir: '../'});
 
   var enterReceipt = read('templates/master.tmpl.html', {breadcrumbs: breadCrumbsExistingReport,
                                                             leftPane: enterReceipt,
                                                             rightPane: table,
-                                                            header: reportHeader});
+                                                            header: reportHeader,
+                                                            rootDir: '../'});
 
   var enterDisbursement = read('templates/master.tmpl.html', {breadcrumbs: breadCrumbsExistingReport,
                                                             leftPane: enterDisbursement,
                                                             rightPane: table,
-                                                            header: reportHeader});
+                                                            header: reportHeader,
+                                                            rootDir: '../'});
 
   var readyToSubmit = read('templates/master.tmpl.html', {breadcrumbs: breadCrumbsExistingReport,
                                                             leftPane: readyToSubmitPane,
                                                             rightPane: table,
-                                                            header: reportHeader});
+                                                            header: reportHeader,
+                                                            rootDir: '../'});
 
 
 
